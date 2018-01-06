936879a1a3f3a5b8a15c164f8e625b753d4dbf90
Sander <sswitsers@gmail.com>
2018-01-06 15:54:34 +0100

    file naming and start 2.5.4

--- part2/realization/HankToSys.m
+++ part2/realization/HankToSys.m
@@ -1,11 +1,10 @@
-function [A, C] = HankToSys(Y)
+function [A, B, C] = HankToSys(Y)
 %HankToSys converts a hankel matrix into an autonomous system
 %
 %   INPUT:
 %   Y = the hankel matrix for the system
 %   OUTPUT:
-%   [A C] = the state space representation of the autonomuos system
-%       which is the minimal state respresentation of the system
+%   [A B C] = the minimal state respresentation of the system
 %       mathcing the hankel matrix
 %
 %Author: SANDER SWITSERS (r0462339)
@@ -24,4 +23,5 @@ O_k = O(1:end-1,:);
 O_ks = O(2:end,:);
 A = pinv(O_k) * O_ks;
 C = O(1,:);
+B = T(:,1);
 end
--- part2/realization/ex_2_5_3.m
+++ part2/realization/ex_2_5_3.m
@@ -1,7 +1,17 @@
+%script that solves and displays the solution of question 2.5.3
+
 Y = hankel(1:10,10:19);
-[A, C] = HankToSys(Y)
+[A, B, C] = HankToSys(Y);
+Disp('A');
+Disp(A);
+Disp('C');
+Disp(C);
 Y = hankel([0 4 6 2 3 3 9 0 4 6 2 3 3 9 0 4 6 2 3 3 9],[9 0 4 6 2 3 3 9 0 4 6 2 3 3 9 0 4 6 2 3 3]);
-[A, C] = HankToSys(Y)
+[A, B, C] = HankToSys(Y);
+Disp('A');
+Disp(A);
+Disp('C');
+Disp(C);
 X = transpose(sym('X',[1 7]));
 eqns = [C*X == 0, C*A*X == 4, C*A^2*X == 6, C*A^3*X == 2, C*A^4*X == 3, C*A^5*X == 3, C*A^6*X == 9];
 sol = solve(eqns,X);
