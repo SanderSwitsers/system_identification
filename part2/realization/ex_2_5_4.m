c2de7f19b3becb933b9e19508794a07438cb5112
Sander <sswitsers@gmail.com>
2018-01-06 21:48:49 +0100

    corrected beginnig of 2.5.4

--- part2/realization/ex_2_5_4.m
+++ part2/realization/ex_2_5_4.m
@@ -1,6 +1,10 @@
-W_k = dlmread('data.txt');
-W_k = transpose(W_k(3:21,2:4));
-W = [W_k;W_k;W_k;W_k;W_k;W_k];
+D = dlmread('data.txt');
+W = zeros(18,19);
+for i=1:6
+   W(3*(i-1)+1,1:19) = D(i:i+18,2)';
+   W(3*(i-1)+2,1:19) = D(i:i+18,3)';
+   W(3*(i-1)+3,1:19) = D(i:i+18,4)';
+end
 W_p = W(1:9,:);
 W_f = W(10:18,:);
 r_W_p = rank(W_p)
