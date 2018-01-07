ccd6330305e76017a5b5671d3e10f0da11c53ecc
Sander <sswitsers@gmail.com>
2018-01-07 22:58:18 +0100

    realization almost done

--- part2/realization/ex_2_5_4.m
+++ part2/realization/ex_2_5_4.m
@@ -10,3 +10,50 @@ W_f = W(10:18,:);
 r_W_p = rank(W_p)
 r_W_f = rank(W_f)
 r_W = rank(W)
+
+%finding a, b and x
+U = transpose(W_p);
+V = transpose(W_f);
+T = null([U V]);
+
+a = T(1:9,1)
+b = T(10:18,1)
+x = transpose(transpose(a1)*W_p)
+
+% input-output subspace realization
+Y = W(3:3:18,:);
+U = W;
+U(3:3:18,:) = [];
+Y_f = Y(4:6,:);
+Y_fm = Y(5:6,:);
+U_f = U(7:12,:);
+U_fm = U(8:12,:);
+U_ii = U(7:8,:);
+Y_ii = Y(4,:);
+W_pp = W(1:12,:);
+W_1 = eye(3);
+W_2 = eye(size(U_f,2)) - transpose(U_f) * pinv(U_f*transpose(U_f)) * U_f;
+
+O_i = obl_proj(Y_f, U_f, W_p);
+O_im = obl_proj(Y_fm, U_fm, W_pp);
+
+[U, S, V] = svd(W_1 * O_i * W_2);
+r = rank(S,1e-8);
+S_1 = S(1:r,1:r);
+U_1 = U(:,1:r);
+
+Gamma = inv(W_1) * U_1 * sqrt(S_1);
+Gamma_m = Gamma(1:2,:);
+
+X_i = pinv(Gamma)*O_i;
+X_ip = pinv(Gamma_m)*O_im;
+
+A = sym('a');
+B = sym('b',[1 2]);
+C = sym('c');
+D = sym('d',[1 2]);
+
+eq1 = X_ip == A*X_i + B * U_ii
+[A, B1, B2] = vpasolve(eq1,[A,B])
+
+%eq = [X_ip;Y_ii] == [A, B; C, D]*[X_i;U_ii]
---
+++ part2/realization/obl_proj.m
@@ -0,0 +1,26 @@
+function result = obl_proj(varargin)
+%G_ESTIMATE Calculates an estimator for g in the eq y=conv(g,u)
+%[g_estimate,cost_ls,cost_aic] = g_estimator(sample_y, sample_u, order, method_name)
+%
+%   INPUT:
+%   A = vector space to be projected
+%   B = vector space along which is projected
+%   C = vector space on which is projected
+%
+%   OUTPUT:
+%   result = the projection of A along B on C.
+%
+%Author: SANDER SWITSERS (r0462339)
+%Date: 07/01/2018
+%Katholieke Universiteit Leuven
+
+    assert(nargin==3,'incorrect number of input arguments');
+    A = varargin{1};
+    B = varargin{2};
+    C = varargin{3};
+    temp = [C*transpose(C) C*transpose(B); B*transpose(C) B*transpose(B)];
+    temp = pinv(temp);
+    temp = temp(:,1:size(C,1));
+    result = A * [transpose(C) transpose(B)] * temp * C;
+    
+end
